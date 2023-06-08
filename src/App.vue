ddb918fb1c4c69f118c2d923f0071eec587ace48
Anthony Destenay <anthony.destenay@gmail.com>
2023-06-08 09:32:18 +0200

    Removed external icon + updated grid

--- src/App.vue
+++ src/App.vue
@@ -32,19 +32,11 @@ function handleFilter() {
     <input type="text" class="input input-lg input-bordered w-full mb-6"
       placeholder="Chercher un streamer ou une streameuse" v-model="filter" @keyup="handleFilter">
     <div v-if="currentStreamers.length === 0" class="text-lg">Aucune streameuse ou streamer trouvé...</div>
-    <ul class="grid grid-cols-5 gap-4">
-      <li v-for="streamer in currentStreamers">
+    <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+      <li v-for="streamer in currentStreamers.slice(0, 100)">
         <a :href="streamer.url" target="_blank" rel="noreferrer"
-          class="btn btn-primary btn-lg btn-block justify-between font-semibold flex-nowrap">
+          class="btn btn-primary btn-lg btn-block font-semibold flex-nowrap">
           <div class="truncate">{{ streamer.login }}</div>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 flex-shrink-0">
-            <path fill-rule="evenodd"
-              d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z"
-              clip-rule="evenodd" />
-            <path fill-rule="evenodd"
-              d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z"
-              clip-rule="evenodd" />
-          </svg>
         </a>
       </li>
     </ul>
